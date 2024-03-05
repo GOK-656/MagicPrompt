416a60e1a53a68a76c8ff02ee394937ba664f48a
wznmickey <first@wznmickey.com>
2024-03-05 15:55:25 +0800

    add 3 model

--- load_drawingModels.py
+++ load_drawingModels.py
@@ -1,21 +1,36 @@
 import requests
 import io
 from PIL import Image
-
-# files saved at diffusion_image.jpeg
-def diffusion_image(inputPromt):
-    API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
-    headers = {"Authorization": "Bearer "}
+from huggerkey import *
+# files saved at image.jpeg
+def useRemoteModel(inputPromt,type:str):
+    if (type=="stable-diffusion"):
+        API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
+    if (type=="midjourney"):
+        API_URL = "https://amidjourneypi-inference.huggingface.co/models/prompthero/openjourney"
+    if (type=="lexica"):
+        API_URL="https://api-inference.huggingface.co/models/openskyml/lexica-aperture-v3-5"
+    if (type=="cascade"): # TODO: have some problem
+        API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-cascade"
+    headers = {"Authorization": "Bearer "+hugging_face_key}
 
     def query(payload):
         response = requests.post(API_URL, headers=headers, json=payload)
         return response.content
-    image_bytes = query({
-        "inputs": inputPromt,
-    })
-    image = Image.open(io.BytesIO(image_bytes))
-    image.save("diffusion_image.jpeg")
+    # sometimes the api is unavailable, TODO: figure unusual cases
+    while True:
+        try:
+            image_bytes = query({
+            "inputs": inputPromt,
+        })
+            print(image_bytes)
+            image = Image.open(io.BytesIO(image_bytes))
+            image.save("image" + type+".jpeg")
+            break
+        except Exception as e:
+            pass
 # # You can access the image with PIL.Image for example
 # import io
 # from PIL import Image
-diffusion_image("A big elephant")
+useRemoteModel("A big elephant","stable-diffusion")
+
