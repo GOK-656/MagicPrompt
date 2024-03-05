3d6aef3ed5899ac611e549fa7ed7baf8713fc83d
GOK-656 <ivanli656@163.com>
2024-03-05 18:34:16 -0500

    fix small bug

--- doinstruct_pix2pix.py
+++ doinstruct_pix2pix.py
@@ -29,7 +29,7 @@ def get_pix2pix_result(inputPromt, savePath, steps=10, text_cfg=7.5, img_cfg=1.5
     # driver = webdriver.Firefox(options=options)
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=chrome_options)
     script = """
