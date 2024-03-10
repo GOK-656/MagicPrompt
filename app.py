5f8b6a492c4c38cc9bfc60a93c23c7b39a393f55
wznmickey <first@wznmickey.com>
2024-03-10 17:25:53 +0000

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import tempfile
 from prompt_generator import *
 from load_drawingModels import *
 import uuid
-
+from gevent import pywsgi
 app = Flask(__name__)
 app.config["TEMP_FOLDER"] = "tmp/"
 
@@ -143,32 +143,41 @@ def search_picture():
 @app.route("/submit_a_picture", methods=["POST", "GET"])
 def submit_a_picture():
     if request.method == "POST":
-        query = request.files["img"]
-        text = image2textData(query)
-        # with tempfile.NamedTemporaryFile() as temp_file:
-        #     query.save(temp_file.name)
-        #     temp_file_path = temp_file.name
-        #     print(temp_file_path)
-        #     img2url(open(temp_file_path, "rb"))
-        img = Image.open(query)
-        if not os.path.exists("tmp"):
-            os.makedirs("tmp")
-        save_path = os.path.join("tmp", query.filename)
-        # save_path = "tmp/" + "tmp." + query.filename.split(".")[-1]
-        print("save path: ", save_path)
-        img.save(save_path)
-        img_stream = ""
-        img_stream = base64.b64encode(open(save_path, "rb").read()).decode("utf-8")
-        # with open(save_path, "rb") as img_file:
-        # img_stream = base64.b64encode(img_file.read()).decode("utf-8")
-        # ans=getResult("Add an eyeglass above the eye")
-        # print(ans)
-        # print(ans[0])
-        # img_stream = get_pix2pix_result("add a bird to sky", save_path)
-        os.remove(save_path)
-        return render_template(
-            "current_picture.html", currentValue=text, pic="0", img_stream=img_stream
-        )
+        try:
+            print("SUBMIT A PICTURE")
+            # print(request.files)
+            # print(request)
+            # print(request.get_data())
+            query = request.files["img"]
+            print("query:img")
+            text = image2textData(query)
+            print("queryText:"+text)
+            # with tempfile.NamedTemporaryFile() as temp_file:
+            #     query.save(temp_file.name)
+            #     temp_file_path = temp_file.name
+            #     print(temp_file_path)
+            #     img2url(open(temp_file_path, "rb"))
+            img = Image.open(query)
+            if not os.path.exists("tmp"):
+                os.makedirs("tmp")
+            save_path = os.path.join("tmp", query.filename)
+            # save_path = "tmp/" + "tmp." + query.filename.split(".")[-1]
+            print("save path: ", save_path)
+            img.save(save_path)
+            img_stream = ""
+            img_stream = base64.b64encode(open(save_path, "rb").read()).decode("utf-8")
+            # with open(save_path, "rb") as img_file:
+            # img_stream = base64.b64encode(img_file.read()).decode("utf-8")
+            # ans=getResult("Add an eyeglass above the eye")
+            # print(ans)
+            # print(ans[0])
+            # img_stream = get_pix2pix_result("add a bird to sky", save_path)
+            os.remove(save_path)
+            return render_template(
+                "current_picture.html", currentValue=text, pic="0", img_stream=img_stream
+            )
+        except Exception as e:
+            print(e)
     return redirect(url_for("home"))
 
 
@@ -297,4 +306,6 @@ def generate():
 
 if __name__ == "__main__":
     # engine = initialize_all()
-    app.run(host="0.0.0.0", port=8080)
+    # app.run(host="0.0.0.0", port=8080)
+    server = pywsgi.WSGIServer(('0.0.0.0', 8080), app)
+    server.serve_forever()
