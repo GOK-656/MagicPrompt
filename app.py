25552154e82b6d9eeb8fdc7a8759e87f750d22a4
lulyany <71319397+lulyany@users.noreply.github.com>
2025-05-10 17:50:06 +0800

    update

--- app.py
+++ app.py
@@ -306,7 +306,7 @@ def generate():
 
 if __name__ == "__main__":
     # engine = initialize_all()
-    # app.run(host="0.0.0.0", port=8080)
-    # app.run(debug=True)
-    server = pywsgi.WSGIServer(("0.0.0.0", 8080), app)
-    server.serve_forever()
+    # app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(debug=True)
+    # server = pywsgi.WSGIServer(("0.0.0.0", 8080), app)
+    # server.serve_forever()
